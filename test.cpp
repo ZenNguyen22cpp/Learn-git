432ca0741ee2aa49271f1098a06be291e39da11b
Phatgus-GID <phatcaothanh.gid@gmail.com>
2026-09-13 22:26:57 +0700

    Add files via upload
    cccc

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define THANH ios_base::sync_with_stdio(false);
+#define PHAT cin.tie(NULL); cout.tie(NULL);
+using namespace std;
+
+signed main(){
+    THANH PHAT
+
+    int n; cin >> n;
+    cout << n * n << '\n';
+}
